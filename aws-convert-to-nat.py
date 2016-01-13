ce053afc58cb3286dc326365f2b59558adb34388
Robert Morris <robert.f.morris@gmail.com>
2016-01-13 16:24:22 -0500

    complete rewrite! and as of this commit, completely untested!
    * more pythonic code
    * reordered the steps so that route table updates happen before
      old NAT instance(s) are terminated ..assuming that's even possible

--- aws-convert-to-nat.py
+++ aws-convert-to-nat.py
@@ -12,218 +12,259 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 import boto3
 import time
+from functools import wraps
 
-client = boto3.client('ec2')
 
-response = client.describe_vpcs()
-vpcs = response['Vpcs']
+def cached_property(fn):
+    """
+    A decorator that returns a property.
 
-if len(vpcs) == 0:
-    print 'You have not VPCs in your account.'
-    exit()
+    The method decorated will only be called the
+    first time the property is computed.
+    """
+    @property
+    @wraps(fn)
+    def _fn(self):
+        key = "_memo_%s" % fn.__name__
+        if not hasattr(self, key):
+            setattr(self, key, fn(self))
+        return getattr(self, key)
+    return _fn
 
-print 'Which VPC would you like to analyze:'
 
-index = 1
-for v in vpcs:
-    name = v['VpcId'] + ' - ' + v['CidrBlock']
-    if v['IsDefault'] == True:
-        name = name + ' (Default VPC)'
-    if 'Tags' in v:
-        if 'Tags' in v:
-            for t in v['Tags']:
+class NoVpcsException(Exception):
+    pass
+
+class VpcHasNatGatewayException(Exception):
+    pass
+
+class NatConverter(object):
+
+    def __init__(self):
+        self.vpc_id = None
+        self.target_subnet = None
+        self.eip_allocation_id = None
+        self.nat_gateway_id = None
+
+    @cached_property
+    def client(self):
+        return boto3.client('ec2')
+
+    @cached_property
+    def vpcs(self):
+        _vpcs = self.client.describe_vpcs()['Vpcs']
+        if not _vpcs:
+            raise NoVpcsException("No VPCs exist on your account")
+        return _vpcs
+
+    def display_vpcs(self):
+        index = 1
+        for v in self.vpcs:
+            name = v['VpcId'] + ' - ' + v['CidrBlock']
+            if v['IsDefault'] == True:
+                name = name + ' (Default VPC)'
+            for t in v.get('Tags', []):
                 if t['Key'] == 'Name':
                     if len(t['Value']) > 0:
                         name = name + ' - ' + t['Value']
                     break
-        # (item for item )
-    print ('{0}:  ' + name).format(index)
-    # print v
-    index += 1
+            print ('[{}] ' + name).format(index)
+            index += 1
 
-input_vpc = raw_input('Enter your number of VPC to analyze: ')
-
-try:
-   val = int(input_vpc)
-except ValueError:
-   print("That's not a valid VPC!1")
-
-if int(input_vpc) < 1 or int(input_vpc) > len(vpcs):
-    print("That's not a valid VPC!")
-    exit()
-
-working_vpcid = vpcs[int(input_vpc)-1]['VpcId']
-
-response = client.describe_nat_gateways(Filter=[{'Name': 'vpc-id', 'Values': [working_vpcid]},{'Name': 'state', 'Values': ['available']}])
-
-if len(response['NatGateways']) > 0:
-    print 'NAT Gateway already exists.'
-    exit()
-
-response = client.describe_route_tables(Filters=[{'Name': 'vpc-id', 'Values': [working_vpcid]}])
-
-print 'Getting Route Tables...'
-
-current_nat_instances = []
-public_subnets = []
-private_subnets = []
-private_route_tables = []
-
-for a in response['RouteTables']:
-    #print 'Route Table-' + a['RouteTableId'] + ':'
-    current_routetable = a['RouteTableId']
-    is_public = False
-    for r in a['Routes']:
-        #print 'Route:'
-        #print r
-        if 'GatewayId' in r:
-            if r['DestinationCidrBlock'] == '0.0.0.0/0':
-                is_public = True;
-        if 'InstanceId' in r:
-            #print r['InstanceId'] + ':' + r['DestinationCidrBlock']
-            dict_to_add = {'RouteTableId': current_routetable, 'InstanceId':r['InstanceId'],'DestinationCidrBlock':r['DestinationCidrBlock']}
-            current_nat_instances.append(dict_to_add.copy())
-    for assoc in a['Associations']:
-        #print 'Associations:'
-        #print assoc
-        if 'SubnetId' in assoc:
-            #print assoc['SubnetId']
-            if is_public == True:
-                public_subnets.append(assoc['SubnetId'])
-            else:
-                private_subnets.append(assoc['SubnetId'])
-    if is_public == False:
-        private_route_tables.append(current_routetable)
-
-#print current_nat_instances
-cni_idx = 0
-for c in current_nat_instances:
-    response = client.describe_instance_attribute(InstanceId=c['InstanceId'],Attribute='sourceDestCheck')
-    if response['SourceDestCheck']['Value'] == True:
-        del current_nat_instances[cni_idx:]
-    cni_idx += 1
-
-if len(current_nat_instances) == 0:
-    print 'There are no NAT instances to replace.  Goodbye!'
-    exit()
-
-print 'current nat instances:'
-print current_nat_instances
-print 'public subnets:'
-print public_subnets
-print 'private subnets: '
-print private_subnets
-print 'private route tables:'
-print private_route_tables
-
-print 'This VPC is validated and VPC NAT Gateway service can replace the legacy NAT instances.'
-print 'The following will take place:'
-print 'Instance ID(s) will be terminated: '
-for instance in current_nat_instances:
-    print instance['InstanceId']
-
-print '\nThe following subnet''s traffic will be routed to the NAT gateway:'
-for sub in private_subnets:
-    print sub
-
-print '\nThe NAT gateway will be deployed into one of the following subnets:'
-for sub in public_subnets:
-    print sub
-
-input_continue = raw_input('Do you wish to continue (Y/n): ')
-
-#try:
-#   val = str(input_continue)
-#except ValueError:
-#   print("That's not a valid choice, goodbye!")
-
-if input_continue != 'Y':
-    print("Goodbye!")
-    exit()
-
-print 'Choose which public subnet to deploy the NAT gateway in:'
-for sub in public_subnets:
-    print sub
-input_pub = raw_input('Type the name of the subnet ID: ')
-
-deploy_subnet = str(input_pub)
-
-input_public_EIP = raw_input('Do you wish to associate an EIP with the NAT Gateway (Y/n): ')
-
-input_final = raw_input('Confirm you want to continue (Y/n): ')
-if input_final != 'Y':
-    print 'Nervous?'
-    exit()
-
-EIP_AllocationID = None
-
-if input_public_EIP == 'Y':
-    response = client.allocate_address(
-        Domain='vpc'
-    )
-    print 'Allociating EIP...'
-    EIP_AllocationID = response['AllocationId']
-
-if EIP_AllocationID == None:
-    print 'no EIP'
-    response = client.create_nat_gateway(
-        SubnetId='string'
-    )
-    print 'Create NAT Gateway...'
-else:
-    print 'EIP'
-    response = client.create_nat_gateway(
-        SubnetId=input_pub,
-        AllocationId=EIP_AllocationID
-    )
-    print 'Create NAT Gateway w/EIP...'
-
-NatGatewayId = response['NatGateway']['NatGatewayId']
-
-for instance in current_nat_instances:
-    print 'terminating:'
-    print instance['InstanceId']
-    response = client.terminate_instances(
-        InstanceIds=[
-            instance['InstanceId']
+    def verify_vpc_is_natless(self, vpc_id=None):
+        f = [
+            {'Name': 'vpc-id', 'Values': [vpc_id or self.vpc_id]},
+            {'Name': 'state', 'Values': ['available']},
         ]
-    )
-    print 'Terminating Old NAT Instances....'
-    # print response
-    # remove old instance from routetables
+        response = self.client.describe_nat_gateways(Filter=f)
+        if len(response['NatGateways']) > 0:
+            raise VpcHasNatGatewayException('NAT Gateway already exists for VPC {}!'.format(vpc_id))
+        return True
 
-    response = client.delete_route(
-        RouteTableId=instance['RouteTableId'],
-        DestinationCidrBlock=instance['DestinationCidrBlock']
-    )
-    print 'Deleting Old Routes...'
-    # print response
+    def select_vpc(self):
+        self.display_vpcs()
+        input_vpc = raw_input('Select the VPC to analyze: ')
+        try:
+            input_vpc = int(input_vpc)
+        except ValueError:
+            print("That's not a valid selection!")
+            self.select_vpc()
+        if input_vpc < 1 or input_vpc > len(self.vpcs):
+            print("That's not a valid selection!")
+            self.select_vpc()
+        self.vpc_id = self.vpcs[input_vpc - 1]['VpcId']
+        self.verify_vpc_is_natless(self.vpc_id)
+        print("Selected VPC: {}".format(self.vpc_id))
+        return self.vpc_id
 
-NAT_Exists = False
-while (NAT_Exists == False):
-    response = client.describe_nat_gateways(
-        NatGatewayIds=[
-            NatGatewayId
+    @cached_property
+    def route_tables(self):
+        f = [
+            {'Name': 'vpc-id', 'Values': [self.vpc_id]},
         ]
-    )
-    # print 'describe_nat_gateways'
-    # print response
+        return self.client.describe_route_tables(Filters=f)['RouteTables']
 
-    if len(response['NatGateways']) > 0:
-        if response['NatGateways'][0]['State'] == 'available':
-            NAT_Exists = True
-    print 'waiting for NAT instance to start....'
+    def public_subnets(self):
+        for route_table in self.route_tables:
+            is_public = False
+            for route in route_table['Routes']:
+                if 'GatewayId' in route and route['DestinationCidrBlock'] == '0.0.0.0/0':
+                    is_public = True
+            for assoc in route_table['Associations']:
+                if 'SubnetId' in assoc and is_public:
+                    #yield assoc['SubnetId']
+                    yield assoc
 
-    time.sleep(5)
+    def private_subnets(self):
+        for route_table in self.route_tables:
+            is_public = False
+            for route in route_table['Routes']:
+                if 'GatewayId' in route and route['DestinationCidrBlock'] == '0.0.0.0/0':
+                    is_public = True
+            for assoc in route_table['Associations']:
+                if 'SubnetId' in assoc and (not is_public):
+                    yield assoc['SubnetId']
 
-for instance in current_nat_instances:
-    response = client.create_route(
-        RouteTableId=instance['RouteTableId'],
-        DestinationCidrBlock=instance['DestinationCidrBlock'],
-        NatGatewayId=NatGatewayId
-    )
-    print 'Creating Route Table Updates...'
-    #print response
+    def nat_instances(self):
+        """
+        All legacy NAT instances on the selected VPC
+        """
+        for route_table in self.route_tables:
+            for route in route_table['Routes']:
+                if 'InstanceId' in route:
+                    yield {
+                        'RouteTableId': route_table,
+                        'InstanceId': route['InstanceId'],
+                        'DestinationCidrBlock': route['DestinationCidrBlock'],
+                    }
 
-print 'Done!'
-exit()
+    def can_nat_be_converted(self, nat_instance):
+        """
+        Apparently NAT instances with src/dest check enabled can't be converted
+        """
+        response = self.client.describe_instance_attribute(InstanceId=nat_instance['InstanceId'], Attribute='sourceDestCheck')
+        return response['SourceDestCheck']['Value'] != True
+
+    def convertable_nat_instances(self):
+        """
+        Only the legacy NAT instances on this VPC that can be replaced with new NAT Gateway instances
+        """
+        return filter(self.can_nat_be_converted, self.nat_instances())
+
+    def allocate_elastic_ip(self):
+        self.eip_allocation_id = self.client.allocate_address(Domain='vpc')['AllocationId']
+
+    def select_target_subnet(self):
+        for sub in self.public_subnets():
+            print sub
+        self.target_subnet = raw_input('Type the ID of the target subnet for the new NAT Gateway: ')
+        return self.target_subnet
+
+    def create_nat_gateway(self):
+        kwargs = {'SubnetId': self.target_subnet}
+        if self.eip_allocation_id:
+            kwargs.update({'AllocationId': self.eip_allocation_id})
+        response = self.client.create_nat_gateway(**kwargs)
+        self.nat_gateway_id = response['NatGateway']['NatGatewayId']
+        return self.nat_gateway_id
+
+    def get_nat_gateway_details(self, nat_gateway_id=None):
+        nat_gateway_id = nat_gateway_id or self.nat_gateway_id
+        f = [
+            {'Name': 'vpc-id', 'Values': [self.vpc_id]},
+        ]
+        return self.client.describe_nat_gateways(
+            NatGatewayIds=[nat_gateway_id],
+            Filter=f,
+        )
+
+    def wait_on_nat_gateways_ready(self, nat_gateway_id):
+        """
+        Wait on all the NAT Gateway service instances on our selected VPC to be ready
+        """
+        print 'Waiting on all NAT Gateway instances to become available',
+        all_gateways_ready = False
+        while not all_gateways_ready:
+            response = self.get_nat_gateway_details(nat_gateway_id=nat_gateway_id)
+            # if *any* instances exist, optimistically assume that we're ready
+            all_gateways_ready = len(response['NatGateways']) > 0
+            for g in response['NatGateways']:
+                # ...but check each instance and make sure
+                if g['State'].lower() != 'available':
+                    all_gateways_ready = False
+            print '.',
+            time.sleep(5)
+        print " all ready!"
+
+    def update_routing(self):
+        print 'Removing route table entries for old NAT instance(s)... ',
+        for instance in self.convertable_nat_instances():
+            response = self.client.delete_route(
+                RouteTableId=instance['RouteTableId'],
+                DestinationCidrBlock=instance['DestinationCidrBlock']
+            )
+        print 'done.'
+        instance = self.get_nat_gateway_details(self.nat_gateway_id)
+        print 'Adding route table entry for new NAT instance... ',
+        response = client.create_route(
+            RouteTableId=instance['RouteTableId'],
+            DestinationCidrBlock=instance['DestinationCidrBlock'],
+            NatGatewayId=NatGatewayId
+        )
+        print 'done.'
+
+    def terminate_legacy_nat_instances(self):
+        print 'Terminating old NAT instances..'
+        for instance in self.convertable_nat_instances():
+            instance_id = instance['InstanceId']
+            print ' terminating instance {}'.format(instance_id)
+            response = client.terminate_instances(
+                InstanceIds=[instance_id]
+            )
+        print ' done.'
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    converter = NatConverter()
+    converter.select_vpc()
+ 
+    print
+    print 'Convertable legacy NAT instances found on this VPC:'
+    for instance in converter.convertable_nat_instances():
+        print '  ', instance['InstanceId']
+
+    print
+    print "This VPC is validated and the AWS VPC NAT Gateway service can replace the existing legacy NAT instances."
+    print
+    print "If you choose to continue, the following actions will be performed:"
+    print " 1. The following legacy NAT instance(s) will be stopped: "
+    for instance in converter.convertable_nat_instances():
+        print '  ', instance['InstanceId']
+
+    print " 2. The following subnets' traffic will be routed to the NAT gateway:"
+    for sub in converter.private_subnets():
+        print '  ', sub
+
+    print " 3. The NAT gateway will be deployed into one of the following subnets (you'll get to select which one):"
+    for sub in converter.public_subnets():
+        print '  ', sub['SubnetId']
+
+    _input = raw_input('Do you wish to continue (Y/n): ')
+    if _input.upper() != 'Y':
+        print("Goodbye!")
+        exit()
+
+    _input = raw_input('Do you wish to associate an EIP with the new NAT Gateway (Y/n): ')
+    if _input.upper() == 'Y':
+        converter.allocate_elastic_ip()
+        print('New EIP allocation id: {}'.format(converter.eip_allocation_id))
+
+    nat_gateway_id = converter.create_nat_gateway()
+    print 'New NAT Gateway instance created: {}'.format(nat_gateway_id)
+
+    converter.wait_on_nat_gateways_ready(nat_gateway_id)
+    converter.update_routing()
+    converter.terminate_legacy_nat_instances()
+
+    print 'Conversion complete!'
